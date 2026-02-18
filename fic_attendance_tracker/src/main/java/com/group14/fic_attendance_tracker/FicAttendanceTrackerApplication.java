fa7b58e8b4f1e012051d0f137ac1a1feb97809cc
groofazy <gabe.gamboa05@gmail.com>
2026-02-18 00:25:46 -0800

    added basic maven project build and Dockerfile config

---
+++ fic_attendance_tracker/src/main/java/com/group14/fic_attendance_tracker/FicAttendanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.group14.fic_attendance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicAttendanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicAttendanceTrackerApplication.class, args);
+	}
+
+}
---
+++ fic_attendance_tracker/src/test/java/com/group14/fic_attendance_tracker/FicAttendanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group14.fic_attendance_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicAttendanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
