7c3d4661d1758346880f6dbcc39040ed40a0b679
groofazy <gabe.gamboa05@gmail.com>
2026-03-28 16:42:07 -0700

    implemented basic user constructor test

--- fic_attendance_tracker/src/test/java/com/group14/fic_attendance_tracker/models/UserTest.java
+++ fic_attendance_tracker/src/test/java/com/group14/fic_attendance_tracker/models/UserTest.java
@@ -1,5 +1,63 @@
 package com.group14.fic_attendance_tracker.models;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+
 public class UserTest {
-    
+
+    @Test
+    void testUserConstructor() {
+        String name = "test";
+        String password = "password";
+        User.RoleType role = User.RoleType.ADMIN;
+
+        User user = new User(name, password , role);
+
+        assertEquals(name, user.getName());
+        assertEquals(password, user.getPassword());
+        assertEquals(role, user.getPassword());
+    }
+
+    @Test
+    void testSetName() {
+        User user = new User();
+        String name = "test";
+        
+        user.setName(name);
+
+        assertEquals(name, user.getName());
+    }
+
+    @Test
+    void testSetPassword() {
+        User user = new User();
+        String password = "test";
+        
+        user.setPassword(password);
+
+        assertEquals(password, user.getPassword());
+    }
+
+    @Test
+    void testSetRole() {
+        User user = new User();
+        User.RoleType role = User.RoleType.ADMIN;
+        
+        user.setRole(role);
+
+        assertEquals(role, user.getRole());
+    }
+
+    @Test
+    void testSetUid() {
+        User user = new User();
+        String name = "test";
+        
+        user.setName(name);
+
+        assertEquals(name, user.getName());
+    }
 }
